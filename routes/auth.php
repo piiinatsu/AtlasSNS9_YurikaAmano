19931ff57169c7d9c35eb3d815aace1442a8e924
Yurika Amano <>
2024-11-15 07:30:39 +0900

    01新規登録/ログイン処理（名前付きルートを設定）

--- routes/auth.php
+++ routes/auth.php
@@ -8,13 +8,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
 
-    Route::get('login', [AuthenticatedSessionController::class, 'create']);
-    Route::post('login', [AuthenticatedSessionController::class, 'store']);
+    // ログインページ
+    Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
+    Route::post('login', [AuthenticatedSessionController::class, 'store'])->name('login.store');
 
-    Route::get('register', [RegisteredUserController::class, 'create']);
-    Route::post('register', [RegisteredUserController::class, 'store']);
+    // 新規登録ページ
+    Route::get('register', [RegisteredUserController::class, 'create'])->name('register');
+    Route::post('register', [RegisteredUserController::class, 'store'])->name('register.store');
 
-    Route::get('added', [RegisteredUserController::class, 'added']);
+    // 新規登録完了ページ
+    Route::get('added', [RegisteredUserController::class, 'added'])->name('register.added');
     Route::post('added', [RegisteredUserController::class, 'added']);
 
 });
