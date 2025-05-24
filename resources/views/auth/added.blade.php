b106c294e20ca48da6679ba01c921aef455e1f7b
Yurika Amano <>
2025-05-24 13:26:58 +0900

    クラスの修正

--- resources/views/auth/added.blade.php
+++ resources/views/auth/added.blade.php
@@ -4,20 +4,20 @@
 
 <div class="background">
   <!-- ロゴとサブタイトル -->
-  <div class="logo-container">
+  <div class="logo_container">
     <img src="{{ asset('images/atlas.png') }}" alt="Atlas Logo" class="logo">
     <h2 class="subtitle">Social Network Service</h2>
   </div>
 
   <!-- 登録完了メッセージ -->
   <section class="added-section">
-    <h2 class="welcome-message">{{ $username }}さん</h2>
+    <h2 class="welcome_message">{{ request()->query('username', 'ゲスト') }}さん</h2>
     <p>ようこそ！AtlasSNSへ！</p>
     <p>ユーザー登録が完了しました。</p>
     <p>早速ログインをしてみましょう。</p>
 
     <div class="login-link-container">
-      <a href="{{ route('login') }}" class="btn-login">ログイン画面へ</a>
+      <a href="{{ route('login') }}" class="btn_login">ログイン画面へ</a>
     </div>
   </section>
 </div>
