30c05ded47716a1e42d2d45d941d5b6f557dd117
Yurika Amano <>
2025-05-25 13:37:50 +0900

    クラス名修正

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,14 +4,14 @@
 
 <div class="background">
     <!-- ロゴとサブタイトル -->
-    <div class="logo-container">
+    <div class="logo_container">
         <img src="{{ asset('images/atlas.png') }}" alt="Atlas Logo" class="logo">
         <h2 class="subtitle">Social Network Service</h2>
     </div>
 
     <!-- 新規登録フォーム -->
-    <section class="register-section">
-        <h2 class="welcome-message">新規ユーザー登録</h2>
+    <section class="register_section">
+        <h2 class="welcome_message">新規ユーザー登録</h2>
         <!-- エラーメッセージの表示 -->
         @if ($errors->any())
             <div class="error-messages">
@@ -26,35 +26,35 @@
         @csrf
 
         <!-- ユーザー名入力 -->
-        <div class="form-group">
-            {{ Form::label('username', 'ユーザー名', ['class' => 'form-label']) }}
+        <div class="form_group">
+            {{ Form::label('username', 'user name', ['class' => 'form_label']) }}
             {{ Form::text('username', null, ['class' => 'input', 'placeholder' => 'ユーザー名を入力']) }}
         </div>
 
         <!-- メールアドレス入力 -->
-        <div class="form-group">
-            {{ Form::label('email', 'メールアドレス', ['class' => 'form-label']) }}
+        <div class="form_group">
+            {{ Form::label('email', 'mail address', ['class' => 'form_label']) }}
             {{ Form::email('email', null, ['class' => 'input', 'placeholder' => 'メールアドレスを入力']) }}
         </div>
 
         <!-- パスワード入力 -->
-        <div class="form-group">
-            {{ Form::label('password', 'パスワード', ['class' => 'form-label']) }}
+        <div class="form_group">
+            {{ Form::label('password', 'password', ['class' => 'form_label']) }}
             {{ Form::password('password', ['class' => 'input', 'placeholder' => 'パスワードを入力']) }}
         </div>
 
         <!-- パスワード確認 -->
-        <div class="form-group">
-            {{ Form::label('password_confirmation', 'パスワード確認', ['class' => 'form-label']) }}
+        <div class="form_group">
+            {{ Form::label('password_confirmation', 'password confirm', ['class' => 'form_label']) }}
             {{ Form::password('password_confirmation', ['class' => 'input', 'placeholder' => 'パスワードを再入力']) }}
         </div>
 
         <!-- 登録ボタン -->
-        <div class="form-group register-button-container">
-            {{ Form::submit('新規登録', ['class' => 'btn-submit']) }}
+        <div class="form_group register_button_container">
+            {{ Form::submit('REGISTER', ['class' => 'btn_submit']) }}
         </div>
 
-        <p class="login-link">
+        <p class="login_link">
             <a href="{{ route('login') }}">ログイン画面へ戻る</a>
         </p>
 
