6a1f52ee5ff9b6ad8212e083d1894f0cbb1644f6
Yurika Amano <>
2025-05-25 20:43:01 +0900

    登録画面レイアウト微修正

--- resources/views/auth/added.blade.php
+++ resources/views/auth/added.blade.php
@@ -6,15 +6,16 @@
   <!-- ロゴとサブタイトル -->
   <div class="logo_container">
     <img src="{{ asset('images/atlas.png') }}" alt="Atlas Logo" class="logo">
-    <h2 class="subtitle">Social Network Service</h2>
+    <h1 class="subtitle">Social Network Service</h1>
   </div>
 
   <!-- 登録完了メッセージ -->
   <section class="added_section">
     <h2 class="welcome_message">{{ request()->query('username', 'ゲスト') }}さん</h2>
     <p>ようこそ！AtlasSNSへ！</p>
-    <p>ユーザー登録が完了しました。</p>
-    <p>早速ログインをしてみましょう。</p>
+    <br/>
+    <p>ユーザー登録が完了いたしました。</p>
+    <p>早速ログインをしてみましょう！</p>
 
     <div class="login_link_container">
       <a href="{{ route('login') }}" class="btn_login">ログイン画面へ</a>
