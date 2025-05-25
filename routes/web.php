8da504be7dd08f7a0a3579e67b63a8ee09f82e93
Yurika Amano <>
2025-05-25 15:23:21 +0900

    余計なコメント削除

--- routes/web.php
+++ routes/web.php
@@ -24,15 +24,12 @@ Route::post('/auth/logout', [AuthenticatedSessionController::class, 'destroy'])-
 
 // ログイン中のみアクセス可能なルートをグループ化
 Route::middleware(['auth'])->group(function () {
-  // トップページ
-  Route::get('posts/index', [PostsController::class, 'index'])->name('posts.index');
   // プロフィールページ
-  // Route::get('/users/profile', [UsersController::class, 'profile'])->name('users.profile');
   Route::get('users/profile', [ProfileController::class, 'profile'])->name('users.profile');
   // プロフィール更新
-  Route::post('/users/profile/update', [UsersController::class, 'updateProfile'])->name('users.updateProfile');
+  Route::post('/users/profile/update', [ProfileController::class, 'update'])->name('users.updateProfile');
+
   // ユーザー検索ページ
-  // Route::get('/users/search', [UsersController::class, 'searchForm'])->name('users.search_form');
   Route::get('users/search', [UsersController::class, 'search'])->name('users.search');
   // 検索結果ページ
   Route::get('users/search_result', [UsersController::class, 'searchResult'])->name('users.search_result');
@@ -43,16 +40,15 @@ Route::middleware(['auth'])->group(function () {
   Route::get('follows/followlist', [FollowsController::class, 'followList'])->name('follows.followlist');
   // フォロワーリストページ
   Route::get('follows/followerlist', [FollowsController::class, 'followerList'])->name('follows.followerlist');
-  // 投稿一覧
+  // 投稿一覧(トップページ)
   Route::get('posts/index', [PostsController::class, 'index'])->name('posts.index');
   // 投稿の作成
   Route::post('posts/store', [PostsController::class, 'store'])->name('posts.store');
   // 投稿の編集
   Route::get('/posts/{id}/edit', [PostsController::class, 'edit'])->name('posts.edit');
   // 投稿の更新
-  Route::post('/posts/{id}/update', [PostsController::class, 'update'])->name('posts.update');
+  Route::put('/posts/{id}/update', [PostsController::class, 'update'])->name('posts.update');
   // 投稿の削除
-  // Route::delete('/posts/{id}', [PostsController::class, 'destroy'])->name('posts.destroy');
   Route::delete('posts/{id}/destroy', [PostsController::class, 'destroy'])->name('posts.destroy');
   // 相手のプロフィール (id付き)
   Route::get('/users/profile/{id}', [UsersController::class, 'showOtherProfile'])->name('users.show');
