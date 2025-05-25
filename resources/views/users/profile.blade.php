569784f3a01b2e80d50b4a39a36722ef7431458f
Yurika Amano <>
2025-05-25 15:14:17 +0900

    クラス名修正

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -6,41 +6,41 @@
     <div class="alert alert-success">{{ session('success') }}</div>
   @endif
 
-  <form action="{{ route('users.updateProfile') }}" method="POST" enctype="multipart/form-data">
+  <form class="profile_form" action="{{ route('users.updateProfile') }}" method="POST" enctype="multipart/form-data">
     @csrf
-    <label>ユーザー名</label>
-    <input type="text" name="username" value="{{ old('username', $user->username) }}">
+    <label class="form_label">ユーザー名</label>
+    <input class="form_input" type="text" name="username" value="{{ old('username', $user->username) }}">
     @error('username')
       <div class="error">{{ $message }}</div>
     @enderror
 
-    <label>メールアドレス</label>
+    <label class="form_label">メールアドレス</label>
     <input type="text" name="mail" value="{{ old('mail', $user->email) }}">
     @error('mail')
       <div class="error">{{ $message }}</div>
     @enderror
 
-    <label>新しいパスワード(変更しない場合は空)</label>
+    <label class="form_label">新しいパスワード(変更しない場合は空)</label>
     <input type="password" name="newPassword">
     @error('newPassword')
       <div class="error">{{ $message }}</div>
     @enderror
 
-    <label>パスワード再入力</label>
+    <label class="form_label">パスワード再入力</label>
     <input type="password" name="newPassword_confirmation">
 
-    <label>自己紹介(bio)</label>
-    <textarea name="bio">{{ old('bio', $user->bio) }}</textarea>
+    <label class="form_label">自己紹介(bio)</label>
+    <textarea class="form_textarea" name="bio">{{ old('bio', $user->bio) }}</textarea>
     @error('bio')
       <div class="error">{{ $message }}</div>
     @enderror
 
-    <label>アイコン画像</label>
+    <label class="form_label">アイコン画像</label>
     <input type="file" name="iconImage">
     @error('iconImage')
       <div class="error">{{ $message }}</div>
     @enderror
 
-    <button type="submit">更新</button>
+    <button class="submit_button" type="submit">更新</button>
   </form>
 </x-login-layout>
