7dad629f062c82735d0a8c3b32b15a8057eb99c2
Yurika Amano <>
2025-05-25 14:51:55 +0900

    クラス名修正

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,11 +1,18 @@
 <x-login-layout>
-  <h1>投稿編集</h1>
-  <form action="{{ route('posts.update', $post->id) }}" method="POST">
-    @csrf
-    <textarea name="post" rows="3">{{ old('post', $post->post) }}</textarea>
-    @error('post')
-      <div class="error">{{ $message }}</div>
-    @enderror
-    <button type="submit">更新</button>
-  </form>
+  <div class="edit_container">
+    <h2>投稿を編集</h2>
+
+    <form method="POST" action="{{ route('posts.update', $post->id) }}">
+      @csrf
+      @method('PUT')
+
+      <!-- 投稿の入力フォーム -->
+      <textarea name="post" class="edit_input" maxlength="150" required>{{ old('post', $post->post) }}</textarea>
+
+      <div class="edit_buttons">
+        <button type="submit" class="update_btn">更新</button>
+        <a href="{{ route('posts.index') }}" class="cancel_btn">キャンセル</a>
+      </div>
+    </form>
+  </div>
 </x-login-layout>
