73f0635fd457342c4f80f7ba8d03db17783debc1
Yurika Amano <>
2025-05-25 13:31:46 +0900

    ログインフォーム　タグ修正

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,37 +1,29 @@
-<!-- LaravelのBladeコンポーネントである -->
-<!-- ファイルは、resources/views/layouts/logout.blade.php に保存されている。 -->
-<!-- 子コンテンツ（フォームや見出し）を $slot に挿入して、動的にHTMLを生成する。 -->
 <x-logout-layout>
 
-<!-- asset() は「公開フォルダ（public/）」の中身を指す便利な関数！ -->
 <link rel="stylesheet" href="{{ asset('css/login.css') }}">
 
 <div class="background">
-  <!-- ロゴとサブタイトル -->
-  <div class="logo-container">
+  <div class="logo_container">
     <img src="{{ asset('images/atlas.png') }}" alt="Atlas Logo" class="logo">
-    <h2 class="subtitle">Social Network Service</h2>
+    <h1 class="subtitle">Social Network Service</h1>
   </div>
-
   <!-- ログインフォーム -->
-  <section class="login-section">
-    <h1 class="welcome-message">AtlasSNSへようこそ</h1>
+  <section class="login_section">
+    <h1 class="welcome_message">AtlasSNSへようこそ</h1>
 
     {!! Form::open(['url' => route('login')]) !!}
-    <!-- <form method="POST" action="http://127.0.0.1:8000/login"> -->
     @csrf
-    <!-- <input type="hidden" name="_token" value="ランダムなトークン"> -->
 
     <!-- メールアドレス入力 -->
-    <div class="form-group">
-      {{ Form::label('email', 'メールアドレス', ['class' => 'form-label']) }}
-      {{ Form::text('email', null, ['class' => 'input', 'placeholder' => 'メールアドレスを入力']) }}
+    <div class="form_group">
+      {{ Form::label('email', 'mail address', ['class' => 'form_label']) }}
+      {{ Form::text('email', null, ['class' => 'input']) }}
     </div>
 
     <!-- パスワード入力 -->
-    <div class="form-group">
-      {{ Form::label('password', 'パスワード', ['class' => 'form-label']) }}
-      {{ Form::password('password', ['class' => 'input', 'placeholder' => 'パスワードを入力']) }}
+    <div class="form_group">
+      {{ Form::label('password', 'password', ['class' => 'form_label']) }}
+      {{ Form::password('password', ['class' => 'input']) }}
     </div>
 
     @if ($errors->any())
@@ -43,8 +35,8 @@
     @endif
 
     <!-- ログインボタン -->
-    <div class="form-group login-button-container">
-      {{ Form::submit('ログイン', ['class' => 'btn-submit']) }}
+    <div class="form_group login-button-container">
+      {{ Form::submit('LOGIN', ['class' => 'btn_submit']) }}
     </div>
 
     <p class="register-link">
