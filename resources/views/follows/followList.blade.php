de8f4a64bff11903fb4ea28b7554bca869afffb8
Yurika Amano <>
2025-05-25 13:57:38 +0900

    アイコン名保存方法変更・クラス名修正

--- resources/views/follows/followList.blade.php
+++ resources/views/follows/followList.blade.php
@@ -1,43 +1,63 @@
 <!-- コンポーネントに変数を渡す -->
 <x-login-layout :username="$username" :followCount="$followCount" :followerCount="$followerCount">
+<link rel="stylesheet" href="{{ asset('css/follows.css') }}">
 
 <div class="container">
+  <div class="follow_header">
+    <h2 class="follow_title">Follow List</h2>
     <!-- アイコン一覧 -->
-    <div class="follower-icons">
-        <div class="icon-list">
-          @if ($followRecords->isNotEmpty())
-            @foreach ($followers as $follower)
-                <a href="{{ route('profile.show', ['id' => $follower->id]) }}">
-                    <img src="{{ $follower->icon_image ?? asset('default-icon.png') }}"
-                      alt="{{ $follower->username }}"
-                      class="user-icon">
-                </a>
+        <div class="icon_list">
+          @if ($followedUsers->isNotEmpty())
+            @foreach ($followedUsers as $follower)
+            @php
+            $iconPath = $follower->icon_image
+              ? (\Illuminate\Support\Str::startsWith($follower->icon_image, 'images/')
+                  ? 'storage/' . $follower->icon_image
+                  : 'storage/images/' . $follower->icon_image)
+              : 'images/default-icon.png';
+            @endphp
+            <a href="{{ route('users.show', ['id' => $follower->id]) }}">
+              <img src="{{ asset($iconPath) }}" alt="{{ $follower->username }}" class="user_icon">
+            </a>
             @endforeach
           @else
               <p>フォローしている人はいません。</p>
           @endif
         </div>
     </div>
-
+    <hr class="section_divider">
     <!-- 投稿一覧 -->
-    <div class="follower-posts">
-      @if ($followerPosts->isNotEmpty())
-        @foreach ($followerPosts as $post)
-            <div class="post">
-                <div class="post-header">
-                    <img src="{{ $post->user->icon_image ?? asset('default-icon.png') }}"
-                      alt="{{ $post->user->username }}"
-                      class="user-icon-small">
-                    <span class="post-username">{{ $post->user->username }}</span>
-                    <span class="post-date">{{ $post->created_at->format('Y-m-d H:i') }}</span>
+    <div class="post_list">
+    <ul>
+      @if ($followingsPosts->isNotEmpty())
+        @foreach ($followingsPosts as $post)
+        @php
+            $iconPath = $post->user->icon_image
+              ? (\Illuminate\Support\Str::startsWith($post->user->icon_image, 'images/')
+                  ? 'storage/' . $post->user->icon_image
+                  : 'storage/images/' . $post->user->icon_image)
+              : 'images/default-icon.png';
+          @endphp
+          <li class="post_block">
+            <figure>
+              <a href="{{ route('users.show', ['id' => $post->user->id]) }}">
+                <img src="{{ asset($iconPath) }}" alt="{{ $post->user->username }}">
+              </a>
+            </figure>
+              <div class="post_content">
+                <div>
+                  <div class="post_name">{{ $post->user->username }}</div>
+                  <div class="post_date">{{ $post->created_at->format('Y-m-d H:i') }}</div>
                 </div>
-                <p class="post-content">{{ $post->post }}</p>
-            </div>
+                <div class="post_text">{{ $post->post }}</div>
+              </div>
+            </li>
         @endforeach
       @else
           <p>フォロー中のユーザーの投稿はありません。</p>
       @endif
-    </div>
+    </ul>
+  </div>
 </div>
 
 </x-login-layout>
